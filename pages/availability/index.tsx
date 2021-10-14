369d66a044df63d5e5e339cb17bb0ca41bc774bf
Felipe Acosta <acosta_jf@icloud.com>
2021-10-14 15:40:05 -0600

    use 12 hour time in availability page

--- pages/availability/index.tsx
+++ pages/availability/index.tsx
@@ -21,15 +21,19 @@ function convertMinsToHrsMins(mins: number) {
   const minutes = m < 10 ? "0" + m : m;
   return `${hours}:${minutes}`;
 }
-export default function Availability() {
+export default function Availability(props) {
+  if (Math.random() > 1) console.log(props);
+
   const queryMe = trpc.useQuery(["viewer.me"]);
   const formModal = useToggleQuery("edit");
 
   const formMethods = useForm<{
     startHours: string;
     startMins: string;
+    startIsPM: string;
     endHours: string;
     endMins: string;
+    endIsPM: string;
     bufferHours: string;
     bufferMins: string;
   }>({});
@@ -43,11 +47,15 @@ export default function Availability() {
     if (formMethods.formState.isDirty || !user) {
       return;
     }
+    const startHoursNum = Number(convertMinsToHrsMins(user.startTime).split(":")[0]);
+    const endHoursNum = Number(convertMinsToHrsMins(user.endTime).split(":")[0]);
     formMethods.reset({
-      startHours: convertMinsToHrsMins(user.startTime).split(":")[0],
+      startHours: String(startHoursNum === 0 ? 12 : startHoursNum > 12 ? startHoursNum - 12 : startHoursNum),
       startMins: convertMinsToHrsMins(user.startTime).split(":")[1],
-      endHours: convertMinsToHrsMins(user.endTime).split(":")[0],
+      startIsPM: String(Number(startHoursNum > 12)),
+      endHours: String(endHoursNum === 0 ? 12 : endHoursNum > 12 ? endHoursNum - 12 : endHoursNum),
       endMins: convertMinsToHrsMins(user.endTime).split(":")[1],
+      endIsPM: String(Number(endHoursNum > 12)),
       bufferHours: convertMinsToHrsMins(user.bufferTime).split(":")[0],
       bufferMins: convertMinsToHrsMins(user.bufferTime).split(":")[1],
     });
@@ -67,10 +75,10 @@ export default function Availability() {
         <div className="flex">
           <div className="w-1/2 mr-2 bg-white border border-gray-200 rounded-sm">
             <div className="px-4 py-5 sm:p-6">
-              <h3 className="text-lg leading-6 font-medium text-gray-900">
+              <h3 className="text-lg font-medium leading-6 text-gray-900">
                 Change the start and end times of your day
               </h3>
-              <div className="mt-2 max-w-xl text-sm text-gray-500">
+              <div className="max-w-xl mt-2 text-sm text-gray-500">
                 <p>
                   Currently, your day is set to start at {convertMinsToHrsMins(user.startTime)} and end at{" "}
                   {convertMinsToHrsMins(user.endTime)}.
@@ -84,10 +92,10 @@ export default function Availability() {
 
           <div className="w-1/2 ml-2 border border-gray-200 rounded-sm">
             <div className="px-4 py-5 sm:p-6">
-              <h3 className="text-lg leading-6 font-medium text-gray-900">
+              <h3 className="text-lg font-medium leading-6 text-gray-900">
                 Something doesn&apos;t look right?
               </h3>
-              <div className="mt-2 max-w-xl text-sm text-gray-500">
+              <div className="max-w-xl mt-2 text-sm text-gray-500">
                 <p>Troubleshoot your availability to explore why your times are showing as they are.</p>
               </div>
               <div className="mt-5">
@@ -104,13 +112,13 @@ export default function Availability() {
           onOpenChange={(isOpen) => {
             router.push(isOpen ? formModal.hrefOn : formModal.hrefOff);
           }}>
-          <DialogContent>
-            <div className="sm:flex sm:items-start mb-4">
-              <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-neutral-100 sm:mx-0 sm:h-10 sm:w-10">
-                <ClockIcon className="h-6 w-6 text-neutral-600" />
+          <DialogContent className="min-w-[420px]">
+            <div className="mb-4 sm:flex sm:items-start">
+              <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 mx-auto rounded-full bg-neutral-100 sm:mx-0 sm:h-10 sm:w-10">
+                <ClockIcon className="w-6 h-6 text-neutral-600" />
               </div>
               <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+                <h3 className="text-lg font-medium leading-6 text-gray-900" id="modal-title">
                   Change your available times
                 </h3>
                 <div>
@@ -122,8 +130,22 @@ export default function Availability() {
             </div>
             <form
               onSubmit={formMethods.handleSubmit(async (values) => {
-                const startMins = parseInt(values.startHours) * 60 + parseInt(values.startMins);
-                const endMins = parseInt(values.endHours) * 60 + parseInt(values.endMins);
+                const startHours12Hour = parseInt(values.startHours);
+                const startHours24Hour = Number(values.startIsPM)
+                  ? startHours12Hour + 12
+                  : startHours12Hour === 12
+                  ? 0
+                  : startHours12Hour;
+
+                const endHours12Hour = parseInt(values.endHours);
+                const endHours24Hour = Number(values.endIsPM)
+                  ? endHours12Hour + 12
+                  : endHours12Hour === 12
+                  ? 0
+                  : endHours12Hour;
+
+                const startMins = startHours24Hour * 60 + parseInt(values.startMins);
+                const endMins = endHours24Hour * 60 + parseInt(values.endMins);
                 const bufferMins = parseInt(values.bufferHours) * 60 + parseInt(values.bufferMins);
 
                 // TODO: Add validation
@@ -145,7 +167,7 @@ export default function Availability() {
                 showToast("The start and end times for your day have been changed successfully.", "success");
               })}>
               <div className="flex mb-4">
-                <label className="w-1/4 pt-2 block text-sm font-medium text-gray-700">Start Time</label>
+                <label className="block w-1/4 pt-2 text-sm font-medium text-gray-700">Start Time</label>
                 <div>
                   <label htmlFor="startHours" className="sr-only">
                     Hours
@@ -154,12 +176,14 @@ export default function Availability() {
                     {...formMethods.register("startHours")}
                     id="startHours"
                     type="number"
-                    className="shadow-sm focus:ring-neutral-500 focus:border-neutral-500 block w-full sm:text-sm border-gray-300 rounded-sm"
+                    className="block w-[6rem] border-gray-300 rounded-sm shadow-sm focus:ring-neutral-500 focus:border-neutral-500 sm:text-sm"
                     placeholder="9"
                     defaultValue={convertMinsToHrsMins(user.startTime).split(":")[0]}
+                    max="12"
+                    min="0"
                   />
                 </div>
-                <span className="mx-2 pt-1">:</span>
+                <span className="pt-1 mx-2 text-gray-900">:</span>
                 <div>
                   <label htmlFor="startMins" className="sr-only">
                     Minutes
@@ -167,14 +191,29 @@ export default function Availability() {
                   <input
                     {...formMethods.register("startMins")}
                     id="startMins"
+                    max="59"
+                    min="0"
                     type="number"
-                    className="shadow-sm focus:ring-neutral-500 focus:border-neutral-500 block w-full sm:text-sm border-gray-300 rounded-sm"
+                    className="block w-[6rem] border-gray-300 rounded-sm shadow-sm focus:ring-neutral-500 focus:border-neutral-500 sm:text-sm"
                     placeholder="30"
                   />
                 </div>
+                <span className="pt-1 mx-2"></span>
+                <div>
+                  <label htmlFor="startAMPM" className="sr-only">
+                    AM/PM
+                  </label>
+                  <select
+                    {...formMethods.register("startIsPM")}
+                    className="block w-[6rem] border-gray-300 rounded-sm shadow-sm focus:ring-neutral-500 focus:border-neutral-500 sm:text-sm"
+                    id="startAMPM">
+                    <option value="0">AM</option>
+                    <option value="1">PM</option>
+                  </select>
+                </div>
               </div>
               <div className="flex mb-4">
-                <label className="w-1/4 pt-2 block text-sm font-medium text-gray-700">End Time</label>
+                <label className="block w-1/4 pt-2 text-sm font-medium text-gray-700">End Time</label>
                 <div>
                   <label htmlFor="endHours" className="sr-only">
                     Hours
@@ -183,11 +222,13 @@ export default function Availability() {
                     {...formMethods.register("endHours")}
                     type="number"
                     id="endHours"
-                    className="shadow-sm focus:ring-neutral-500 focus:border-neutral-500 block w-full sm:text-sm border-gray-300 rounded-sm"
-                    placeholder="17"
+                    max="12"
+                    min="0"
+                    className="block w-[6rem] border-gray-300 rounded-sm shadow-sm focus:ring-neutral-500 focus:border-neutral-500 sm:text-sm"
+                    placeholder="5"
                   />
                 </div>
-                <span className="mx-2 pt-1">:</span>
+                <span className="pt-1 mx-2 text-gray-900">:</span>
                 <div>
                   <label htmlFor="endMins" className="sr-only">
                     Minutes
@@ -195,14 +236,29 @@ export default function Availability() {
                   <input
                     {...formMethods.register("endMins")}
                     type="number"
+                    max="59"
+                    min="0"
                     id="endMins"
-                    className="shadow-sm focus:ring-neutral-500 focus:border-neutral-500 block w-full sm:text-sm border-gray-300 rounded-sm"
+                    className="block w-[6rem] border-gray-300 rounded-sm shadow-sm focus:ring-neutral-500 focus:border-neutral-500 sm:text-sm"
                     placeholder="30"
                   />
                 </div>
+                <span className="pt-1 mx-2"></span>
+                <div>
+                  <label htmlFor="endAMPM" className="sr-only">
+                    AM/PM
+                  </label>
+                  <select
+                    {...formMethods.register("endIsPM")}
+                    className="block w-[6rem] border-gray-300 rounded-sm shadow-sm focus:ring-neutral-500 focus:border-neutral-500 sm:text-sm"
+                    id="endAMPM">
+                    <option value="0">AM</option>
+                    <option value="1">PM</option>
+                  </select>
+                </div>
               </div>
               <div className="flex mb-4">
-                <label className="w-1/4 pt-2 block text-sm font-medium text-gray-700">Buffer</label>
+                <label className="block w-1/4 pt-2 text-sm font-medium text-gray-700">Buffer</label>
                 <div>
                   <label htmlFor="bufferHours" className="sr-only">
                     Hours
@@ -211,11 +267,11 @@ export default function Availability() {
                     {...formMethods.register("bufferHours")}
                     type="number"
                     id="bufferHours"
-                    className="shadow-sm focus:ring-neutral-500 focus:border-neutral-500 block w-full sm:text-sm border-gray-300 rounded-sm"
+                    className="block w-[6rem] border-gray-300 rounded-sm shadow-sm focus:ring-neutral-500 focus:border-neutral-500 sm:text-sm"
                     placeholder="0"
                   />
                 </div>
-                <span className="mx-2 pt-1">:</span>
+                <span className="pt-1 mx-2 text-gray-900">:</span>
                 <div>
                   <label htmlFor="bufferMins" className="sr-only">
                     Minutes
@@ -224,12 +280,12 @@ export default function Availability() {
                     {...formMethods.register("bufferMins")}
                     type="number"
                     id="bufferMins"
-                    className="shadow-sm focus:ring-neutral-500 focus:border-neutral-500 block w-full sm:text-sm border-gray-300 rounded-sm"
+                    className="block w-[6rem] border-gray-300 rounded-sm shadow-sm focus:ring-neutral-500 focus:border-neutral-500 sm:text-sm"
                     placeholder="10"
                   />
                 </div>
               </div>
-              <div className="mt-5 sm:mt-4 sm:flex space-x-2">
+              <div className="mt-5 space-x-2 sm:mt-4 sm:flex">
                 <Button href={formModal.hrefOff} color="secondary" tabIndex={-1}>
                   Cancel
                 </Button>
