69658d4a07fe54a107fdb3465b2cb2defebf9d62
Felipe Acosta <acosta_jf@icloud.com>
2021-11-23 15:27:01 -0700

    log

--- pages/api/availability/[user].ts
+++ pages/api/availability/[user].ts
@@ -56,7 +56,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (!rawUser) throw new Error("No user found");
 
   const { selectedCalendars, ...currentUser } = rawUser;
-  currentUser.credentials.map(console.log);
+  currentUser.credentials.map((x) => console.log({ x }));
   const busyTimes = await getBusyCalendarTimes(
     currentUser.credentials,
     dateFrom.format(),
