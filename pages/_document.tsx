8b8b53dcb65b53756cd5664c8d261ddcdf94cb89
Felipe Acosta <acosta_jf@icloud.com>
2022-01-16 20:38:03 -0700

    update msapplication tile color

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,7 +18,7 @@ class MyDocument extends Document<Props> {
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
-          <meta name="msapplication-TileColor" content="#ff0000" />
+          <meta name="msapplication-TileColor" content="#F3C93C" />
           <meta name="theme-color" content="#F3C93C" media="(prefers-color-scheme: light)" />
           <meta name="theme-color" content="#141414" media="(prefers-color-scheme: dark)" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
